984b882944442003c3e39d294811c8edc08fce98
spyraws <82212218+spyraws@users.noreply.github.com>
2024-12-10 00:16:58 +0100

    Update test_voiture.py

--- tests/test_voiture.py
+++ tests/test_voiture.py
@@ -7,7 +7,7 @@ import os
 # Ajouter 'src' au sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from projet_github import Voiture
+
 from src.projet_github import Voiture  
 
 
